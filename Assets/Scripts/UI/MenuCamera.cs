1e3c57420798e01732e6724b0c47adfc96114822
Richard Ivanek <richard.ivanek@gmail.com>
2020-04-22 00:56:19 +0200

    Menu

---
+++ Assets/Scripts/UI/MenuCamera.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuCamera : MonoBehaviour
+{
+    [Range(-60, 60)] public float YSpeed;
+    [Range(-60, 60)] public float ZSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    float yAngle;
+    float zAngle;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.localRotation = Quaternion.Euler(0, yAngle = yAngle + Time.deltaTime * YSpeed, zAngle = zAngle + Time.deltaTime * ZSpeed);
+    }
+}
---
+++ Assets/Scripts/UI/MenuControlls.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuControlls : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void StartGame()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Scenes/Game", UnityEngine.SceneManagement.LoadSceneMode.Single);
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
