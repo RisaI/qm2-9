1f8c2692cc4e5613c339e5358c3b4f72786dc10c
Richard Ivanek <richard.ivanek@gmail.com>
2020-04-21 22:49:41 +0200

    Quest item strikethrough and UI

---
+++ Assets/Scripts/UI/QuestLine.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestLine : MonoBehaviour
+{
+    [TextArea(1,3)] public string Text;
+    [TextArea(1,3)] public string StrikedText;
+
+    [Range(0, 10)]
+    public int MinStage;
+
+    public bool _Striked;
+    public bool Striked {
+        get { return _Striked; }
+        set {
+            _Striked = value;
+            SetText();
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetText();
+    }
+
+    void SetText()
+    {
+        GetComponent<TMPro.TextMeshProUGUI>().text = _Striked ? StrikedText : Text;
+    }
+}
---
+++ Assets/Scripts/UI/QuestList.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestList : MonoBehaviour
+{
+    public QuestLine[] QuestLines;
+
+    [Range(0, 10)]
+    public int _Stage;
+    public int Stage
+    {
+        get { return _Stage; }
+        set {
+            _Stage = value;
+            RecalculateState();
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        RecalculateState();
+    }
+
+    void RecalculateState()
+    {
+        foreach (var line in QuestLines)
+            line.Striked = _Stage >= line.MinStage;
+    }
+}
