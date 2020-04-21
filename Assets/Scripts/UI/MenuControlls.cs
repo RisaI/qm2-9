507b61844043fa7ed71a87ef38ac43c48ddc722d
Richard Ivanek <richard.ivanek@gmail.com>
2020-04-22 01:32:33 +0200

    Menu drive button

--- Assets/Scripts/UI/MenuControlls.cs
+++ Assets/Scripts/UI/MenuControlls.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class MenuControlls : MonoBehaviour
 {
+    public bool DriveLocked;
+    public UnityEngine.UI.Image DriveImage;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        DriveImage.enabled = DriveLocked;
     }
 
     public void StartGame()
     {
         UnityEngine.SceneManagement.SceneManager.LoadScene("Scenes/Game", UnityEngine.SceneManagement.LoadSceneMode.Single);
+    }
+
+    public void OnDriveClicked()
+    {
+        if (!DriveLocked)
+        {
+            Application.OpenURL("https://www.seznam.cz/");
+        }
     }
 
     public void Exit()
